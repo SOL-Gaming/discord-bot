64c9c12edcf7fc8e6275857d07cc2298e1e34408
0w9 <79471472+0w9@users.noreply.github.com>
2022-03-18 20:48:01 +0100

    Removed old comments, to clean the code up.

--- commands/verify.js
+++ commands/verify.js
@@ -19,20 +19,10 @@ module.exports = {
 		const verificationEmbed = new MessageEmbed()
 			.setColor('#0099ff')
 			.setTitle('Holder Verification')
-			//.setURL('https://discord.js.org/')
-			//.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
 			.setDescription('We are happy you are here!\nAs next step it is important that you verify your NFT. To do this, visit the given link below and connect the account to your Phantom Wallet.\nAfter you did this, interact with the button to get the roles.')
-			//.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-			/*.addFields(
-				{ name: 'Regular field title', value: 'Some value here' },
-				{ name: '\u200B', value: '\u200B' },
-				{ name: 'Inline field title', value: 'Some value here', inline: true },
-				{ name: 'Inline field title', value: 'Some value here', inline: true },
-			)*/
 			.addField('Verify here 🔗', '[Link](https://127.0.0.1:2001) to the verification.', true)
-			//.setImage('https://i.imgur.com/AfFp7pu.png')
 			.setTimestamp()
-			.setFooter({ text: 'Bunny Fight Club', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+			.setFooter({ text: 'lennardeth' });
 
 	await interaction.reply({ embeds: [verificationEmbed], components: [row] });
 
